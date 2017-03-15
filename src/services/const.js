45953dad7d74c16b131e3cc6d1d0650972194cac
Johan Armando <johinsdev@gmail.com>
2017-03-15 10:33:38 -0500

    Url production

--- src/services/const.js
+++ src/services/const.js
@@ -1,5 +1,5 @@
 
-export const API_URL = 'http://localhost:3001/';
+export const API_URL = 'http://dev.mocionsoft.com:6478/';
 export const LOGIN_URL = API_URL + 'auth/';
 export const SIGNUP_URL = API_URL + 'users/';
 export const TIKECT_URL = API_URL + 'tickets';
