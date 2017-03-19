b294998c1b21091c68ef17c7e8c9201fb77cc39b
Nicolas  Restrepo <nicolasrestrepo@MacBook-Pro-de-Nicolas.local>
2017-03-19 10:36:43 -0700

    design

--- src/App.vue
+++ src/App.vue
@@ -100,9 +100,16 @@ body{
         
         height: 150px;
         width: 100%;
-        background: #5682A3;
+      
+
+  background: red; /* For browsers that do not support gradients */
+  background: -webkit-linear-gradient(left top, red, yellow); /* For Safari 5.1 to 6.0 */
+  background: -o-linear-gradient(bottom right, red, yellow); /* For Opera 11.1 to 12.0 */
+  background: -moz-linear-gradient(bottom right, red, yellow); /* For Firefox 3.6 to 15 */
+  background: linear-gradient(to bottom right, red, yellow); /* Standard syntax */
     }
     
+
     .logo img {
         width: 400px;
         display: block;
